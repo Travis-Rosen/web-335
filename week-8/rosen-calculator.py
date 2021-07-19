4c3c26df755017689e92a4ce5e95944345202fa3
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-07-18 19:25:17 -0500

    Added Week 8 File

---
+++ week-8/rosen-calculator.py
@@ -0,0 +1,25 @@
+#Travis Rosen
+#Rosen-Calculator.py
+#7/18/2021
+#Simple math equations using python
+
+#This function will use addition to combine two numbers. 
+num1 = 10
+num2 = 10
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#This function will use subtraction to see the difference between two numbers.
+x = 100
+y = 75
+z = x - y
+print(z)
+
+#This function will use division on two numbers.
+a = 250
+b = 15
+print('a / b =',a/b)
+
+
+
+
